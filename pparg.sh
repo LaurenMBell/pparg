9d33b6dd1df7647a7cc41736111ebf450e4460c9
LaurenMBell <bell.laurenmar@gmail.com>
2026-01-11 11:14:37 -0800

    cleaning up

--- pparg.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "================= PPARG DRUG EFFECTIVENESS PREDICTION ====================="
-read-p "Input target molecule in SMILES format (ex: COc1ccc([C@@H](O)[C@H](O)CO)cc1OC): " TARGET
-
-
-
-
-
-
